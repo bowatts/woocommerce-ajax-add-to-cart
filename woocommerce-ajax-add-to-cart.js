f8ec2b053234ad3713b04b32b816f5f4f8baf4c7
Bo Watts <bo@bowatts.com>
2015-05-22 09:18:51 -0500

    Added condition for using arrows to change qty

--- woocommerce-ajax-add-to-cart.js
+++ woocommerce-ajax-add-to-cart.js
@@ -10,5 +10,16 @@ jQuery( document ).ready(function() {
 			jQuery('input.qty').val('1');
 		}
 	});
+	
+	jQuery('input.qty').change(function() {
+		var quantity = jQuery('input.qty').val();
+		if( jQuery.isNumeric( quantity ) ) {
+			jQuery( '.add_to_cart_button' ).attr( 'data-quantity', quantity );
+		}
+		else {
+			jQuery( '.add_to_cart_button' ).attr( 'data-quantity', '1' );
+			jQuery('input.qty').val('1');
+		}
+	});
 
 });
